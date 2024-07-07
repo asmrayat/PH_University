580ad5b4720c7bc7ebc1d4c7dd4dff2b56a8c89d
asmrayat <asnrayat@gmail.com>
2024-07-07 19:14:49 +0600

    mod-1

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Main_layout from "./components/layout/Main_layout";
+
+function App() {
+  return <Main_layout />;
+}
+
+export default App;
---
+++ src/pages/admin/AdminDeshboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminDeshboard = () => {
+    return (
+        <div>
+            <h1> this is admin page</h1>
+        </div>
+    );
+};
+
+export default AdminDeshboard;
